913cb9d1bb72c2b5f29c89970142cbf8b65964a9
Thepython555 <144301348+thepython555@users.noreply.github.com>
2024-01-03 16:52:28 -0800

    trying to fix pencil

--- api/main.py
+++ api/main.py
@@ -1,14 +1,56 @@
 from flask import Flask, render_template, request
 #import requests
 import scratchattach as scratch3
-import scratchrandom
-import existence
+import requests
+
 
 app = Flask(__name__)
 
+#EXISTENCE & SCRATCHRANDOM CUZ VERCEL HATES IT
+
+def check(id):
+    try:
+        notfoundtext = requests.get("https://api.scratch.mit.edu/projects/7676767676767676").text
+        thistext = requests.get(f"https://api.scratch.mit.edu/projects/{str(id)}").text
+        if thistext == notfoundtext:
+            return False
+        else:
+            return True
+    except Exception:
+        return False
+
+def checkUser(username):
+    notfoundtext = requests.get("https://api.scratch.mit.edu/users/aAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/").text
+    thistext = requests.get(f"https://api.scratch.mit.edu/users/{username}").text
+    if thistext == notfoundtext:
+        return False
+    else:
+        return True
+
+def getRandomProjects(amount):
+    arr = []
+    for i in range(int(amount)):
+        arr.append(getRandomProject())
+    return arr
+
+def getRandomProject():
+    try:
+        running = True
+        notfoundtext = requests.get("https://api.scratch.mit.edu/projects/7676767676767676").text
+        projectcount = 908838871
+        while running:
+            idrandom = random.randint(0,projectcount)
+            urlrandom = f"https://api.scratch.mit.edu/projects/{idrandom}"
+            if requests.get(urlrandom).text != notfoundtext:
+                return idrandom
+    except Exception:
+        return 0
+
+
+
 def getRandomProjects():
     arr = []
-    for item in scratchrandom.getRandomProjects(1):
+    for item in getRandomProjects(1):
         arr.append(scratch3.get_project(item))
     return arr
 
@@ -18,14 +60,14 @@ def index():
 
 @app.route("/projects/<id>")
 def project(id):
-    if not existence.check(id):
+    if not check(id):
         return render_template("projecterror.html")
     saproject = scratch3.get_project(id)
     return render_template("project.html", title=saproject.title, author=saproject.author, id=saproject.id, instructions=saproject.instructions, notes=saproject.notes, comments=saproject.comments(limit=999, offset=0))
 
 @app.route("/users/<username>")
 def userpage(username):
-    if not existence.checkUser(username):
+    if not checkUser(username):
         return render_template("usererror.html")
     sauser = scratch3.get_user(username)
     return render_template("user.html", user=sauser, username=username, users_projects=sauser.projects(limit=None, offset=0), fav_projects=sauser.favorites(limit=None, offset=0), following=sauser.following(limit=999, offset=0), followers=sauser.followers(limit=999, offset=0), comments=sauser.comments(page=1, limit=999))
